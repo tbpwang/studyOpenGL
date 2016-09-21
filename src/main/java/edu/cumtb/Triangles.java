252ac60563d99a318dde76de57f3c68812972e8a
tbpwang <tbpwang@gmail.com>
2016-09-21 09:10:22 +0800

    Initial commit

---
+++ src/main/java/edu/cumtb/Triangles.java
@@ -0,0 +1,25 @@
+package edu.cumtb;
+
+/**
+ * Created by tbpwang@gmail.com
+ * 2016/9/21.
+ */
+public class Triangles {
+    enum VAO_IDs{Triangles, NumVAOs}
+    enum Buffer_IDs{ArrayBuffer, NumBuffers}
+    enum Attrib_IDs{vPosition}
+
+//    int VAOs[VAO_IDs.NumVAOs]
+    public static final int NUMVERTICES = 6;
+    public void init(){
+
+    }
+
+    public void display(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
